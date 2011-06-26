f6173c32081204298aa4bc44663b35e229ba925d
Jay Nelson <jay@duomark.com>
2011-06-26 07:30:20 -0700

    Add dialzyer specs to mq_speed

--- msg/mq_speed/src/mq_speed.erl
+++ msg/mq_speed/src/mq_speed.erl
@@ -9,11 +9,17 @@
 %% Application callbacks
 %% ===================================================================
 
+-spec start() -> {ok, pid()} | {error, any()} | ignore.
+-spec start(any(), any()) -> {ok, pid()} | {error, any()} | ignore.
+-spec stop([]) -> ok.
+
+%% @doc Start the application's root supervisor in erl listener.
 start() ->
     mq_speed_sup:start_link().
 
+%% @doc Start the application's root supervisor from boot.
 start(_StartType, _StartArgs) ->
     mq_speed_sup:start_link().
 
-stop(_State) ->
-    ok.
+%% @doc Stop the application.
+stop(_State) -> ok.
